a2bd6a516487dc0b3f99e979fe50490e0945ff00
dufedanceq <103373187+dufedanceq@users.noreply.github.com>
2024-12-24 12:46:11 +0200

    added Task class

--- lab5.js
+++ lab5.js
@@ -1,1 +1,56 @@
 const EventEmitter = require("events");
+
+class Task {
+  constructor(id, processTime, eventEmitter) {
+    this.id = id;
+    this.processTime = processTime;
+    this.status = "idle";
+    this.eventEmitter = eventEmitter;
+
+    this.eventEmitter.on("startTask", (taskId) => {
+      if (taskId === this.id) {
+        this.start();
+      }
+    });
+
+    this.eventEmitter.on("abortAll", () => {
+      this.abort();
+    });
+  }
+
+  async start() {
+    if (this.status !== "idle") return;
+
+    this.status = "running";
+    this.eventEmitter.emit("taskStatus", { id: this.id, status: "running" });
+
+    try {
+      await new Promise((resolve) => setTimeout(resolve, this.processTime));
+
+      if (this.status === "aborted") {
+        return;
+      }
+
+      this.status = "completed";
+      this.eventEmitter.emit("taskStatus", {
+        id: this.id,
+        status: "completed",
+      });
+    } catch (error) {
+      this.status = "failed";
+      this.eventEmitter.emit("taskStatus", {
+        id: this.id,
+        status: "failed",
+        error,
+      });
+    }
+  }
+
+  abort() {
+    if (this.status === "running") {
+      this.status = "aborted";
+      this.eventEmitter.emit("taskStatus", { id: this.id, status: "aborted" });
+    }
+  }
+}
+
